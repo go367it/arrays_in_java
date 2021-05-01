72eeb37f75a75144bca030f37356a28f708a0198
go367it <satyamkumar3001@gmail.com>
2021-05-01 13:36:41 +0530

    initial commit

---
+++ src/AddArrayElements.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class AddArrayElements {
+
+    // main function
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+
+}
