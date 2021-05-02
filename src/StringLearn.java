90eb2efa3a080eb6f9af97a6adbe593357341e30
go367it <satyamkumar3001@gmail.com>
2021-05-02 16:31:55 +0530

    implemented String

---
+++ src/StringLearn.java
@@ -0,0 +1,7 @@
+public class StringLearn {
+    public static void main(String[] args){
+        String s = "Coding";
+        System.out.println(s.length());
+        System.out.println(s.charAt(2));
+    }
+}
